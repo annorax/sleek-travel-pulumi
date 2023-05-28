54b5c96fd0fef7ee9d640d0c0a7057f1038694c8
annorax <ido.dovrat@gmail.com>
2023-05-28 11:17:08 +0300

    Create VPC

--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as dotenv from "dotenv";
+import { Subnet } from "@pulumi/aws/ec2";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+dotenv.config();
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const vpc = new awsx.ec2.Vpc("slim-travel");
+
+pulumi.all([vpc.subnets, vpc.privateSubnetIds]).apply(([subnets, privateSubnetIds]) => {
+    const subnetsById:{ [key: string]: Subnet } = {};
+    pulumi.all(subnets.map(subnet => subnet.id)).apply(subnetIds => {
+        for (let i = 0; i < subnets.length; i++) {
+            subnetsById[subnetIds[i]] = subnets[i];
+        }
+        const privateSubnets = privateSubnetIds.map(privateSubnetId => subnetsById[privateSubnetId]);
+        pulumi.all(privateSubnets.map(privateSubnet => privateSubnet.availabilityZone)).apply(privateSubnetAvailabilityZones => {
+            console.log(privateSubnetAvailabilityZones);
+            console.log([...new Set(privateSubnetAvailabilityZones)]);
+        });
+    });
+});
+
+export = async () => {
+    
+}
