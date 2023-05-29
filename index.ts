78d34320e49aece3268f508668c5df275687cfa9
annorax <ido.dovrat@gmail.com>
2023-05-29 10:05:35 +0300

    Create Aurora cluster

--- index.ts
+++ index.ts
@@ -1,26 +1,27 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import * as dotenv from "dotenv";
 import { Subnet } from "@pulumi/aws/ec2";
 
 dotenv.config();
 
-const vpc = new awsx.ec2.Vpc("slim-travel");
-
-pulumi.all([vpc.subnets, vpc.privateSubnetIds]).apply(([subnets, privateSubnetIds]) => {
-    const subnetsById:{ [key: string]: Subnet } = {};
-    pulumi.all(subnets.map(subnet => subnet.id)).apply(subnetIds => {
-        for (let i = 0; i < subnets.length; i++) {
-            subnetsById[subnetIds[i]] = subnets[i];
-        }
-        const privateSubnets = privateSubnetIds.map(privateSubnetId => subnetsById[privateSubnetId]);
-        pulumi.all(privateSubnets.map(privateSubnet => privateSubnet.availabilityZone)).apply(privateSubnetAvailabilityZones => {
-            console.log(privateSubnetAvailabilityZones);
-            console.log([...new Set(privateSubnetAvailabilityZones)]);
-        });
-    });
-});
+const numberOfAvailabilityZones = 2;
 
 export = async () => {
-    
+    const vpc = new awsx.ec2.Vpc("slim-travel", {
+        numberOfAvailabilityZones: numberOfAvailabilityZones
+    });    
+    const allAvailabilityZones = await aws.getAvailabilityZones(undefined, { parent: vpc });
+    const availabilityZones = allAvailabilityZones.names.slice(0, numberOfAvailabilityZones);
+    const postgresql = new aws.rds.Cluster("postgresql", {
+        availabilityZones: availabilityZones,
+        backupRetentionPeriod: 365,
+        clusterIdentifier: "slim-travel",
+        databaseName: "slim-travel",
+        engine: "aurora-postgresql",
+        masterUsername: <string>process.env.POSTGRESQL_USERNAME,
+        masterPassword: <string>process.env.POSTGRESQL_PASSWORD,
+        preferredBackupWindow: "07:00-09:00"
+    });
 }
