0f14e9b7b8a1acf6382ddc25c702c43dc7ef9c99
annorax <ido.dovrat@gmail.com>
2023-05-29 14:24:23 +0300

    Create client VPN endpoing

--- index.ts
+++ index.ts
@@ -1,25 +1,34 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import * as dotenv from "dotenv";
-import { Subnet } from "@pulumi/aws/ec2";
 
 dotenv.config();
 
-const numberOfAvailabilityZones = 2;
+const availabilityZoneNames = ['eu-west-2a', 'eu-west-2b'];
 
 export = async () => {
     const vpc = new awsx.ec2.Vpc("slim-travel", {
-        numberOfAvailabilityZones: numberOfAvailabilityZones
-    });    
-    const allAvailabilityZones = await aws.getAvailabilityZones(undefined, { parent: vpc });
-    const availabilityZones = allAvailabilityZones.names.slice(0, numberOfAvailabilityZones);
+        availabilityZoneNames: availabilityZoneNames
+    });
     const dbSubnetGroup = new aws.rds.SubnetGroup("dbsubnet", {
         name: "slim-travel",
         subnetIds: vpc.privateSubnetIds,
     });
-    const postgresql = new aws.rds.Cluster("postgresql", {
-        availabilityZones: availabilityZones,
+    new aws.ec2clientvpn.Endpoint("slim-travel-clientvpn", {
+        vpcId: vpc.vpcId,
+        serverCertificateArn: "arn:aws:acm:eu-west-2:486087129309:certificate/feb470b7-caa5-45a8-935f-146e7ef4eecd",
+        clientCidrBlock: "10.1.0.0/16",
+        authenticationOptions: [{
+            type: "certificate-authentication",
+            rootCertificateChainArn: "arn:aws:acm:eu-west-2:486087129309:certificate/fa995600-1a33-4fa4-b9da-c3124f440431",
+        }],
+        connectionLogOptions: {
+            enabled: false
+        },
+        splitTunnel: true
+    });
+    new aws.rds.Cluster("slim-travel", {
+        availabilityZones: availabilityZoneNames,
         dbSubnetGroupName: dbSubnetGroup.name,
         backupRetentionPeriod: 35,
         clusterIdentifier: "slim-travel",
@@ -28,5 +37,7 @@ export = async () => {
         masterUsername: <string>process.env.POSTGRESQL_USERNAME,
         masterPassword: <string>process.env.POSTGRESQL_PASSWORD,
         preferredBackupWindow: "07:00-09:00"
+    }, {
+        ignoreChanges: ["availabilityZones"]
     });
 }
