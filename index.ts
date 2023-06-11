815aec6bd7b7bcd833587d53cbaa92ef21536e1d
annorax <ido.dovrat@gmail.com>
2023-06-11 21:15:38 +0300

    Partially migrate to aws-native pulumi provider

--- index.ts
+++ index.ts
@@ -1,61 +1,64 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
+import * as awsClassic from "@pulumi/aws";
+import * as aws from "@pulumi/aws-native";
 import * as awsx from "@pulumi/awsx";
 import * as dotenv from "dotenv";
-import { Subnet } from "@pulumi/aws/ec2";
 
 dotenv.config();
 
 const vpcCidrBlock = "10.0.0.0/16";
 const dbEngine = "aurora-postgresql";
+const baseName = "slim-travel";
 
 const availabilityZoneNames = ['eu-west-2a', 'eu-west-2b'];
 
 const vpnEndpointLogGroupName = "vpn/endpoint";
-const vpnEndpointLogStreamName = "slim-travel";
+const vpnEndpointLogStreamName = baseName;
 
 export = async () => {
-    const vpc = new awsx.ec2.Vpc("slim-travel", {
+    const vpc = new awsx.ec2.Vpc(baseName, {
         availabilityZoneNames: availabilityZoneNames,
         cidrBlock: vpcCidrBlock,
         enableDnsSupport: true,
         enableDnsHostnames: true
     });
-    const dbSubnetGroup = new aws.rds.SubnetGroup("dbsubnet", {
-        name: "slim-travel",
+    const subnetGroup = new aws.rds.DBSubnetGroup(baseName, {
+        dBSubnetGroupName: baseName,
+        dBSubnetGroupDescription: baseName,
         subnetIds: vpc.privateSubnetIds,
     });
-    const cluster = new aws.rds.Cluster("slim-travel", {
+    const cluster = new aws.rds.DBCluster(baseName, {
         availabilityZones: availabilityZoneNames,
-        dbSubnetGroupName: dbSubnetGroup.name,
+        dBSubnetGroupName: baseName,
+        port: 5432,
         backupRetentionPeriod: 35,
-        clusterIdentifier: "slim-travel",
+        dBClusterIdentifier: baseName,
         databaseName: "SlimTravel",
         engine: dbEngine,
         engineVersion: "15.2",
         masterUsername: <string>process.env.POSTGRESQL_USERNAME,
-        masterPassword: <string>process.env.POSTGRESQL_PASSWORD,
-        preferredBackupWindow: "07:00-09:00",
-        skipFinalSnapshot: true
+        masterUserPassword: <string>process.env.POSTGRESQL_PASSWORD,
+        preferredBackupWindow: "07:00-09:00"
     }, {
-        ignoreChanges: ["availabilityZones"]
+        ignoreChanges: ["availabilityZones"],
+        dependsOn: [ subnetGroup ]
     });
     for (let i = 0; i < availabilityZoneNames.length; i++) {
-        new aws.rds.ClusterInstance(`slim-travel-${i + 1}`, {
-            clusterIdentifier: cluster.id,
+        new aws.rds.DBInstance(`${baseName}-${i + 1}`, {
+            dBClusterIdentifier: cluster.id,
             engine: dbEngine,
-            instanceClass: aws.rds.InstanceType.T3_Medium,
+            dBInstanceClass: "db.t3.medium",
             availabilityZone: availabilityZoneNames[i]
         });
     }
-    const logGroup = new aws.cloudwatch.LogGroup(vpnEndpointLogGroupName, {
+    const logGroup = new awsClassic.cloudwatch.LogGroup(vpnEndpointLogGroupName, {
         name: vpnEndpointLogGroupName
     });
-    const logStream = new aws.cloudwatch.LogStream(vpnEndpointLogStreamName, {
+    const logStream = new awsClassic.cloudwatch.LogStream(vpnEndpointLogStreamName, {
         name: vpnEndpointLogStreamName,
         logGroupName: logGroup.name
     });
-    const clientVpnEndpoint = new aws.ec2clientvpn.Endpoint("slim-travel-clientvpn", {
+    const clientVpnEndpoint = new awsClassic.ec2clientvpn.Endpoint(`${baseName}-clientvpn`, {
         vpcId: vpc.vpcId,
         serverCertificateArn: "arn:aws:acm:eu-west-2:486087129309:certificate/feb470b7-caa5-45a8-935f-146e7ef4eecd",
         clientCidrBlock: "10.1.0.0/16",
@@ -75,19 +78,19 @@ export = async () => {
     });
     vpc.privateSubnetIds.apply(privateSubnetIds => {
         for (let i = 0; i < privateSubnetIds.length; i++) {
-            new aws.ec2clientvpn.NetworkAssociation(`slim-travel-${i+1}`, {
+            new awsClassic.ec2clientvpn.NetworkAssociation(`${baseName}-${i+1}`, {
                 clientVpnEndpointId: clientVpnEndpoint.id,
                 subnetId: privateSubnetIds[i],
             });
         }
     });
-    new aws.ec2clientvpn.AuthorizationRule("slim-travel", {
+    new awsClassic.ec2clientvpn.AuthorizationRule(baseName, {
         clientVpnEndpointId: clientVpnEndpoint.id,
         targetNetworkCidr: vpcCidrBlock,
         authorizeAllGroups: true
     });
     pulumi.all([vpc.subnets, vpc.privateSubnetIds]).apply(([subnets, privateSubnetIds]) => {
-        const subnetsById:{ [key: string]: Subnet } = {};
+        const subnetsById:{ [key: string]: awsClassic.ec2.Subnet } = {};
         pulumi.all(subnets.map(subnet => subnet.id)).apply(subnetIds => {
             for (let i = 0; i < subnets.length; i++) {
                 subnetsById[subnetIds[i]] = subnets[i];
@@ -95,7 +98,7 @@ export = async () => {
             const privateSubnets = privateSubnetIds.map(privateSubnetId => subnetsById[privateSubnetId]);
             pulumi.all(privateSubnets.map(privateSubnet => privateSubnet.cidrBlock)).apply(privateSubnetCidrBlocks => {
                 for (let i = 0; i < privateSubnetCidrBlocks.length; i++) {
-                    new aws.ec2clientvpn.Route(`slim-travel-${i + 1}`, {
+                    new awsClassic.ec2clientvpn.Route(`${baseName}-${i + 1}`, {
                         clientVpnEndpointId: clientVpnEndpoint.id,
                         destinationCidrBlock: "0.0.0.0/0",
                         targetVpcSubnetId: privateSubnets[i].id,
